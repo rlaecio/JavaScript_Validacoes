d98728a1c5cb0ff8b4c047653f0d3a87dec1b86e
rlaecio <rlaecio@hotmail.com>
2023-03-18 10:48:59 +0000

    comparar se maior ou meno

--- js/validacao.js
+++ js/validacao.js
@@ -3,12 +3,14 @@ function cerificaChute(chute) {
 
     if(chuteForValido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
     }
 
     if (numeroMaioOuMenor(numero)) {
         elementoChute.innerHTML += `<
             div>Valor inválido: Fale um numero entre 
             ${menorValor} e ${maiorValor}</div>`
+            return
     }
 
     if(numero === numeroSecreto) {
@@ -16,7 +18,17 @@ function cerificaChute(chute) {
             <h2> Você acertou !</h2>
             <h3> O numero secreto era ${numeroSecreto}</h3>
         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O numero secreto é menor <i class="fa-solid fa-arrow-dow"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
     }
+
+
 }
 
 function chuteForValido(numero) {
